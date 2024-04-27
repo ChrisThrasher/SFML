dcec4a70e3d5aa01f836965892f13cf63cbdb007
Chris Thrasher <chrisjthrasher@gmail.com>
2024-04-26 23:24:28 -0600

    Remove redundant bounds checks

--- src/SFML/Window/DRM/InputImpl.cpp
+++ src/SFML/Window/DRM/InputImpl.cpp
@@ -549,9 +549,6 @@ namespace sf::priv::InputImpl
 bool isKeyPressed(Keyboard::Key key)
 {
     const std::lock_guard lock(inputMutex);
-    if ((static_cast<int>(key) < 0) || (static_cast<int>(key) >= static_cast<int>(keyMap.size())))
-        return false;
-
     update();
     return keyMap[key];
 }
@@ -604,9 +601,6 @@ void setVirtualKeyboardVisible(bool /*visible*/)
 bool isMouseButtonPressed(Mouse::Button button)
 {
     const std::lock_guard lock(inputMutex);
-    if ((static_cast<int>(button) < 0) || (static_cast<int>(button) >= static_cast<int>(mouseMap.size())))
-        return false;
-
     update();
     return mouseMap[button];
 }
