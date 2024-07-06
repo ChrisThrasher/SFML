cee12b76f589ef6b5665bd1f7d996b1065ee1076
Chris Thrasher <chrisjthrasher@gmail.com>
2024-07-05 23:02:24 -0600

    Add more Audio module tests

--- test/Audio/SoundRecorder.test.cpp
+++ test/Audio/SoundRecorder.test.cpp
@@ -1,9 +1,35 @@
 #include <SFML/Audio/SoundRecorder.hpp>
 
+#include <catch2/catch_test_macros.hpp>
+
 #include <type_traits>
 
-static_assert(!std::is_constructible_v<sf::SoundRecorder>);
-static_assert(!std::is_copy_constructible_v<sf::SoundRecorder>);
-static_assert(!std::is_copy_assignable_v<sf::SoundRecorder>);
-static_assert(!std::is_nothrow_move_constructible_v<sf::SoundRecorder>);
-static_assert(!std::is_nothrow_move_assignable_v<sf::SoundRecorder>);
+namespace
+{
+struct SoundRecorder : sf::SoundRecorder
+{
+    [[nodiscard]] bool onProcessSamples(const std::int16_t* /* samples*/, std::size_t /* sampleCount */) override
+    {
+        return true;
+    }
+};
+} // namespace
+
+TEST_CASE("[Audio] sf::SoundRecorder")
+{
+    SECTION("Type traits")
+    {
+        STATIC_CHECK(!std::is_constructible_v<sf::SoundRecorder>);
+        STATIC_CHECK(!std::is_copy_constructible_v<sf::SoundRecorder>);
+        STATIC_CHECK(!std::is_copy_assignable_v<sf::SoundRecorder>);
+        STATIC_CHECK(!std::is_nothrow_move_constructible_v<sf::SoundRecorder>);
+        STATIC_CHECK(!std::is_nothrow_move_assignable_v<sf::SoundRecorder>);
+    }
+
+    SECTION("Construction")
+    {
+        const SoundRecorder soundRecorder;
+        CHECK(soundRecorder.getSampleRate() == 44100);
+        CHECK(soundRecorder.getChannelCount() == 1);
+    }
+}
