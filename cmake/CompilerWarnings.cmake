a8f2ad72ec47b1e282a3754f195a76414e7232ea
Chris Thrasher <chrisjthrasher@gmail.com>
2024-08-20 19:36:54 -0600

    Reenable some warnings

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -33,8 +33,6 @@ function(set_target_warnings target)
 
             # Disables, remove when appropriate
             /wd4068 # disable warnings about unknown pragmas (e.g. #pragma GCC)
-            /wd4505 # disable warnings about unused functions that might be platform-specific
-            /wd4800 # disable warnings regarding implicit conversions to bool
             /wd4275 # disable warnings about exporting non DLL-interface classes
         )
     endif()
