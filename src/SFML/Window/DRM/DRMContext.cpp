ec9a101a121a3bcc83d6b78530493cf7f1ede040
Chris Thrasher <chrisjthrasher@gmail.com>
2024-10-04 15:15:58 -0600

    Preincrement when possible

--- src/SFML/Window/DRM/DRMContext.cpp
+++ src/SFML/Window/DRM/DRMContext.cpp
@@ -511,7 +511,7 @@ namespace sf::priv
 ////////////////////////////////////////////////////////////
 DRMContext::DRMContext(DRMContext* shared)
 {
-    contextCount++;
+    ++contextCount;
 
     // Get the initialized EGL display
     m_display = getInitializedDisplay();
@@ -533,7 +533,7 @@ DRMContext::DRMContext(DRMContext* shared)
 ////////////////////////////////////////////////////////////
 DRMContext::DRMContext(DRMContext* shared, const ContextSettings& settings, const WindowImpl& owner, unsigned int bitsPerPixel)
 {
-    contextCount++;
+    ++contextCount;
 
     // Get the initialized EGL display
     m_display = getInitializedDisplay();
@@ -553,7 +553,7 @@ DRMContext::DRMContext(DRMContext* shared, const ContextSettings& settings, cons
 ////////////////////////////////////////////////////////////
 DRMContext::DRMContext(DRMContext* shared, const ContextSettings& settings, Vector2u size)
 {
-    contextCount++;
+    ++contextCount;
 
     // Get the initialized EGL display
     m_display = getInitializedDisplay();
