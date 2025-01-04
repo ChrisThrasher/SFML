40283123ee8ef92329a06f0311f1e1268dbbf7ea
Chris Thrasher <chrisjthrasher@gmail.com>
2025-01-04 11:27:32 -0700

    Use Catch2 to implement benchmarking program

--- benchmarks/Benchmark.cpp
+++ benchmarks/Benchmark.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
 
-#include <chrono>
+#include <catch2/benchmark/catch_benchmark_all.hpp>
+#include <catch2/catch_test_macros.hpp>
+
 #include <iostream>
 #include <random>
 
-int main()
+TEST_CASE("Benchmark")
 {
     // Set up scene
     sf::RenderWindow               window(sf::VideoMode({720, 480}), "Benchmark");
@@ -12,28 +14,26 @@ int main()
     std::uniform_real_distribution xDist(0.f, static_cast<float>(window.getSize().x));
     std::uniform_real_distribution yDist(0.f, static_cast<float>(window.getSize().y));
     std::uniform_int_distribution  pixelDist(0, 255);
-    std::vector<sf::Texture>       textures;
-    textures.reserve(2'500);
-    std::vector<sf::Sprite> sprites;
-    sprites.reserve(textures.capacity());
-    for (std::size_t i = 0; i < textures.capacity(); ++i)
+    std::vector<sf::Texture>       textures(2'500);
+    std::vector<sf::Sprite>        sprites;
+    sprites.reserve(textures.size());
+    for (auto& texture : textures)
     {
-        auto& texture = textures.emplace_back(
+        REQUIRE(texture.loadFromImage(
             sf::Image(sf::Vector2u(8, 8),
                       sf::Color(static_cast<std::uint8_t>(pixelDist(rng)),
                                 static_cast<std::uint8_t>(pixelDist(rng)),
                                 static_cast<std::uint8_t>(pixelDist(rng)),
-                                static_cast<std::uint8_t>(pixelDist(rng)))));
+                                static_cast<std::uint8_t>(pixelDist(rng))))));
         auto& sprite = sprites.emplace_back(texture);
         sprite.setPosition({xDist(rng), yDist(rng)});
     }
+    REQUIRE(sprites.size() == textures.size());
 
     // Perform benchmark
-    constexpr auto maxRuntime = std::chrono::seconds(5);
-    auto           eventCount = 0;
-    auto           frameCount = 0;
-    const auto     start      = std::chrono::steady_clock::now();
-    while (std::chrono::steady_clock::now() - start < maxRuntime)
+    auto eventCount = 0;
+    auto frameCount = 0;
+    BENCHMARK("Main loop")
     {
         while (const std::optional event = window.pollEvent())
             ++eventCount;
@@ -44,27 +44,12 @@ int main()
         window.display();
 
         ++frameCount;
-    }
-    const auto runtime = std::chrono::steady_clock::now() - start;
+    };
+    CHECK(frameCount > 0);
+    CHECK(eventCount < frameCount); // Fewer than 1 event per frame is to be expected
 
     // Show results
-    struct CommaNumpunct : std::numpunct<char>
-    {
-        [[nodiscard]] char do_thousands_sep() const override
-        {
-            return ',';
-        }
-        [[nodiscard]] std::string do_grouping() const override
-        {
-            return "\3";
-        }
-    };
-    const auto runtimeInSeconds = std::chrono::duration_cast<std::chrono::duration<float>>(runtime).count();
-    const auto averageFramerate = static_cast<float>(frameCount) / runtimeInSeconds;
-    std::cout.imbue(std::locale(std::locale::classic(), new CommaNumpunct));
-    std::cout << std::fixed << std::setprecision(1);
-    std::cout << "Event count\t" << eventCount << "\n";
-    std::cout << "Frame count\t" << frameCount << "\n";
-    std::cout << "Runtime:\t" << runtimeInSeconds << " seconds\n";
-    std::cout << "Framerate:\t" << averageFramerate << " fps\n";
+    std::cout << '\n';
+    std::cout << "Event count: " << eventCount << "\n";
+    std::cout << "Frame count: " << frameCount << "\n";
 }
